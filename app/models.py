71eecfda523c2126c4fb77eed776031f006f297b
Germán Rojas <g.rojasc@uniandes.edu.co>
2023-04-24 08:38:45 -0500

    User's password

--- app/models.py
+++ app/models.py
@@ -1,13 +1,37 @@
 # Create your models here.
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-class User(models.Model):
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('El email es obligatorio')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser):
     name = models.CharField(max_length=50, null=False, blank=False)
     lastname = models.CharField(max_length=50, null=False, blank=False)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=50, null=False, blank=False)
     address = models.CharField(max_length=50, null=False, blank=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'lastname', 'phone', 'address']
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return f'Full Name: {self.name} {self.lastname} - Email: {self.email}'
