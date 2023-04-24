ba747b23e4aad731332b2fb35e0c3af5cae0594b
Germán Rojas <g.rojasc@uniandes.edu.co>
2023-04-23 20:48:32 -0500

    Transaction class with its subclasses (Expense and Income)

--- expenseControl/models.py
+++ expenseControl/models.py
@@ -17,3 +17,42 @@ class User(models.Model):
     def getAddress(self):
         return self.address
     
+
+class Transaction(models.Model):
+    TRANSACTION_TYPE_CHOICES = (
+        ('income', 'Income'),
+        ('expense', 'Expense'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=255)
+    date = models.DateField()
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPE_CHOICES)
+
+    def __str__(self):
+        return f'{self.transaction_type.capitalize()} - {self.description}'
+
+    def clean(self):
+        if self.transaction_type == 'income' and self.amount < 0:
+            raise ValidationError("The amount of an income must be positive")
+        elif self.transaction_type == 'expense' and self.amount > 0:
+            raise ValidationError("The amount of an expense must be negative")
+
+
+class Income(Transaction):
+    class Meta:
+        proxy = True
+
+    def clean(self):
+        if self.amount < 0:
+            raise ValidationError("The amount of an income must be positive")
+
+
+class Expense(Transaction):
+    class Meta:
+        proxy = True
+
+    def clean(self):
+        if self.amount > 0:
+            raise ValidationError("The amount of an expense must be negative")
